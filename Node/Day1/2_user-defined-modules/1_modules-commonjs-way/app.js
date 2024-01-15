dcb936eab71623e33a928c2d8bf7f2a0e699b5b5
manish.sharma <manish.sharma@technizerindia.com>
2024-01-15 14:42:37 +0530

    Node Modules

---
+++ Node/Day1/2_user-defined-modules/1_modules-commonjs-way/app.js
@@ -0,0 +1,12 @@
+// Import an entire module for side effects only, without importing anything. 
+// This runs the module's global code, but doesn't actually import any values.
+// require('./lib.js');
+
+// const lib = require('./lib.js');
+// console.log(lib);
+
+// console.log(lib.firstname);
+// console.log(lib.lastname);
+// console.log(lib.hello('Hello World'));
+
+// --------- Load and instantiate the Employee Object  
---
+++ Node/Day1/2_user-defined-modules/1_modules-commonjs-way/lib.js
@@ -0,0 +1,37 @@
+// console.log('This is the lib module');
+// console.log(module);
+
+// var fname = "Manish";
+// module.exports = fname;
+
+// var lname = "Sharma";
+// module.exports = lname;
+
+// var fname = "Manish";
+// var lname = "Sharma";
+
+// module.exports = { fname, lname };
+
+// ------------------------- Named Exports
+// var fname = "Manish";
+// var lname = "Sharma";
+
+// module.exports.firstname = fname;
+// module.exports.lastname = lname;
+
+// module.exports.hello = function (message) {
+//     return `From Lib - ${message.toUpperCase()}`;
+// }
+
+// ------------------------- Named Exports - Shorter Syntax
+// var fname = "Manish";
+// var lname = "Sharma";
+
+// exports.firstname = fname;
+// exports.lastname = lname;
+
+// exports.hello = function (message) {
+//     return `From Lib - ${message.toUpperCase()}`;
+// }
+
+// --------- Create and Export Employee Class
